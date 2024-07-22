24cd4c79d52390b2f929beadcdfefd7745c992ea
SuggarHidden <68570464+SuggarHidden@users.noreply.github.com>
2024-07-22 13:00:13 -0500

    filter by playername and last messager

--- src/main/java/me/suggarhidden/privatemessages/commands/Message.java
+++ src/main/java/me/suggarhidden/privatemessages/commands/Message.java
@@ -14,69 +14,74 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 public class Message implements SimpleCommand {
-    @Override
-    public void execute(SimpleCommand.Invocation invocation) {
-        CommandSource source = invocation.source();
+  @Override
+  public void execute(SimpleCommand.Invocation invocation) {
+    CommandSource source = invocation.source();
 
-        if (!(source instanceof Player)) {
-            source.sendMessage(MiniMessage.miniMessage().deserialize("Only players can use this command."));
-            return;
-        }
-
-        Player sender = (Player) source;
-        String[] args = invocation.arguments();
-
-        if (args.length < 2) {
-            source.sendMessage(MiniMessage.miniMessage().deserialize(
-                    PrivateMessages.getConfig().getString("messages.message_usage")));
-            return;
-        }
-
-        if (args[0].equalsIgnoreCase(sender.getUsername())) {
-            sender.sendMessage(MiniMessage.miniMessage().deserialize(
-                    PrivateMessages.getConfig().getString("messages.message_self")));
-            return;
-        }
-
-        Optional<Player> targetOpt = PrivateMessages.getProxy().getPlayer(args[0]);
-        if (targetOpt.isEmpty()) {
-            source.sendMessage(MiniMessage.miniMessage().deserialize(
-                    PrivateMessages.getConfig().getString("messages.player_not_found")));
-            return;
-        }
-
-        Player target = targetOpt.get();
-        String msg = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
-
-        sender.sendMessage(MiniMessage.miniMessage().deserialize(
-                PrivateMessages.getConfig().getString("format.sender"),
-                Placeholder.parsed("sender", sender.getUsername()),
-                Placeholder.parsed("receiver", target.getUsername()),
-                Placeholder.unparsed("message", msg)));
-
-        target.sendMessage(MiniMessage.miniMessage().deserialize(
-                PrivateMessages.getConfig().getString("format.receiver"),
-                Placeholder.parsed("sender", sender.getUsername()),
-                Placeholder.parsed("receiver", target.getUsername()),
-                Placeholder.unparsed("message", msg)));
-
-        PrivateMessages.getRecentMessages().put(sender.getUniqueId(), target.getUniqueId());
+    if (!(source instanceof Player)) {
+      source.sendMessage(MiniMessage.miniMessage().deserialize("Only players can use this command."));
+      return;
     }
 
-    @Override
-    public boolean hasPermission(SimpleCommand.Invocation invocation) {
-        return invocation.source().hasPermission(PrivateMessages.getConfig().getString("permissions.message"));
+    Player sender = (Player) source;
+    String[] args = invocation.arguments();
+
+    if (args.length < 2) {
+      source.sendMessage(
+              MiniMessage.miniMessage().deserialize(PrivateMessages.getConfig().getString("messages.message_usage")));
+      return;
     }
 
-    @Override
-    public CompletableFuture<List<String>> suggestAsync(SimpleCommand.Invocation invocation) {
-        List<String> players = new ArrayList<>();
-        String[] args = invocation.arguments();
+    if (args[0].equalsIgnoreCase(sender.getUsername())) {
+      sender.sendMessage(
+              MiniMessage.miniMessage().deserialize(PrivateMessages.getConfig().getString("messages.message_self")));
+      return;
+    }
 
-        if (args.length < 2) {
-            PrivateMessages.getProxy().getAllPlayers().forEach(p -> players.add(p.getUsername()));
+    Optional<Player> targetOpt = PrivateMessages.getProxy().getPlayer(args[0]);
+    if (targetOpt.isEmpty()) {
+      source.sendMessage(MiniMessage.miniMessage()
+              .deserialize(PrivateMessages.getConfig().getString("messages.player_not_found")));
+      return;
+    }
+
+    Player target = targetOpt.get();
+    String msg = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
+
+    sender.sendMessage(MiniMessage.miniMessage().deserialize(PrivateMessages.getConfig().getString("format.sender"),
+            Placeholder.parsed("sender", sender.getUsername()), Placeholder.parsed("receiver", target.getUsername()),
+            Placeholder.unparsed("message", msg)));
+
+    target.sendMessage(MiniMessage.miniMessage().deserialize(PrivateMessages.getConfig().getString("format.receiver"),
+            Placeholder.parsed("sender", sender.getUsername()), Placeholder.parsed("receiver", target.getUsername()),
+            Placeholder.unparsed("message", msg)));
+
+    PrivateMessages.getRecentMessages().put(sender.getUniqueId(), target.getUniqueId());
+    if (PrivateMessages.getConfig().getBoolean("last_message_reply")) PrivateMessages.getRecentMessages().put(target.getUniqueId(), sender.getUniqueId());
+  }
+
+  @Override
+  public boolean hasPermission(SimpleCommand.Invocation invocation) {
+    return invocation.source().hasPermission(PrivateMessages.getConfig().getString("permissions.message"));
+  }
+
+  @Override
+  public CompletableFuture<List<String>> suggestAsync(SimpleCommand.Invocation invocation) {
+    List<String> players = new ArrayList<>();
+    String[] args = invocation.arguments();
+
+    if (args.length > 1) {
+      String filter = args[1].toLowerCase();
+
+      PrivateMessages.getProxy().getAllPlayers().forEach(p -> {
+        if (p.getUsername().toLowerCase().startsWith(filter)) {
+          players.add(p.getUsername());
         }
-
-        return CompletableFuture.completedFuture(players);
+      });
+    } else {
+      PrivateMessages.getProxy().getAllPlayers().forEach(p -> players.add(p.getUsername()));
     }
+
+    return CompletableFuture.completedFuture(players);
+  }
 }
